419a3ca8e00155d1d03869e2c0a09ac4402d7b7c
Caleb-Script <caleb-script@outlook.de>
2026-03-13 16:58:03 +0100

    fix(update): fix CurrentUser

--- src/decorators/current-user.decorator.ts
+++ src/decorators/current-user.decorator.ts
@@ -25,13 +25,14 @@ export const CurrentUser = createParamDecorator(
     }
 
     const user= req.user;
+    const userInfo = user.raw
 
     return {
-      id: user.sub,
-      username: user.preferred_username,
+      id: user.sub ?? userInfo.sub,
+      username: user.preferred_username ?? userInfo.preferred_username,
       email: user.email,
-      firstName: user.given_name,
-      lastName: user.family_name,
+      firstName: user.given_name ?? userInfo.given_name,
+      lastName: user.family_name ?? userInfo.family_name,
       roles: extractUserRoles(user.raw),
       access_token: user.access_token,
       refresh_token: user.refresh_token,
